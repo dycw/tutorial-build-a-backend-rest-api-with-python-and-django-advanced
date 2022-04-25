e2b35f0fde85f81a2ff5f16e056d102264724ecf
Derek Wan <d.wan@icloud.com>
2022-04-25 23:41:43 +0800

    Initialize app

---
+++ app/app/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+from os import environ
+
+from django.core.wsgi import get_wsgi_application
+
+
+_ = environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+
+application = get_wsgi_application()
