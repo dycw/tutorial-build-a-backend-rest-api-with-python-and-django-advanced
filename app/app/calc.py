ac9e253abd20886da6ab8f21d9c0c26520f4ad70
Derek Wan <d.wan@icloud.com>
2022-04-26 21:55:20 +0800

    update

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,6 @@
 def add(x: int, y: int) -> int:
     return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    return x - y
--- app/app/test_foo.py
+++ app/app/test_foo.py
@@ -1,7 +1,11 @@
 from app.calc import add
+from app.calc import subtract
 from django.test import TestCase
 
 
-class TestAdd(TestCase):
+class TestCalc(TestCase):
     def test_add(self) -> None:
         self.assertEqual(add(1, 1), 2)
+
+    def test_subtract(self) -> None:
+        self.assertEqual(subtract(2, 1), 1)
