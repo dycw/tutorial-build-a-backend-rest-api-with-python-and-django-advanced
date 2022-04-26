5568d639790ab2f63357e715cbd0a8fc92d51e88
Derek Wan <d.wan@icloud.com>
2022-04-27 06:47:30 +0800

    Finish section 9 (#6)
    * Bump version: 0.0.6 → 0.0.7
    
    * Set up PG
    
    * Add postgres to docker
    
    * Finish section 9

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,19 @@ FROM	python:3.10-alpine
 LABEL	maintainer="Derek Wan <d.wan@icloud.com>"
 ENV	PYTHONUNBUFFERED	1
 COPY	./requirements.txt	/requirements.txt
+RUN	apk add --no-cache \
+	--update \
+	postgresql-client
+RUN	apk add --no-cache \
+	--update \
+	--virtual \
+	.tmp-apk \
+	gcc \
+	libc-dev \
+	linux-headers \
+	postgresql-dev
 RUN	pip install -r /requirements.txt
+RUN	apk del .tmp-apk
 RUN	mkdir /app
 WORKDIR	/app
 COPY	./app	/app
--- app/app/settings.py
+++ app/app/settings.py
@@ -81,11 +81,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": getenv("DB_HOST"),
+        "NAME": getenv("DB_NAME"),
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
