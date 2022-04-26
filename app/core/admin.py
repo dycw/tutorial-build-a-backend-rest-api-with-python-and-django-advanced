93aca1a51b49f2b2627111ad3434febf4df3e325
Derek Wan <d.wan@icloud.com>
2022-04-26 23:50:37 +0800

    Finish section 8

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,30 @@
-# Register your models here.
+from core.models import User
+from django.contrib.admin import site
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        (gettext("Personal Info"), {"fields": ["name"]}),
+        (
+            gettext("Permissions"),
+            {"fields": ["is_active", "is_staff", "is_superuser"]},
+        ),
+        (gettext("Important dates"), {"fields": ["last_login"]}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2"],
+            },
+        )
+    ]
+
+
+site.register(User, UserAdmin)
