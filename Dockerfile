26b6e0808b3d3caf5e983ab9f5c6e957fa45b59b
Derek Wan <d.wan@icloud.com>
2022-04-30 00:16:58 +0800

    Finish section 15 (#12)
    * Add pillow as requirement
    
    * update dockerfile
    
    * Create core migration
    
    * Finish section 15

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ ENV	PYTHONUNBUFFERED	1
 COPY	./requirements.txt	/requirements.txt
 RUN	apk add --no-cache \
 	--update \
-	postgresql-client
+	postgresql-client \
+	jpeg-dev
 RUN	apk add --no-cache \
 	--update \
 	--virtual \
@@ -12,11 +13,18 @@ RUN	apk add --no-cache \
 	gcc \
 	libc-dev \
 	linux-headers \
-	postgresql-dev
+	postgresql-dev \
+	musl-dev \
+	zlib \
+	zlib-dev
 RUN	pip install -r /requirements.txt
 RUN	apk del .tmp-apk
 RUN	mkdir /app
 WORKDIR	/app
 COPY	./app	/app
+RUN	mkdir -p /vol/web/media
+RUN	mkdir -p /vol/web/static
 RUN	adduser -D user
+RUN	chown -R user:user /vol
+RUN	chmod -R 755 /vol/web
 USER	user
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,12 +7,17 @@ from core.models import Tag
 from django.db.models.query import QuerySet
 from recipe.serializers import IngredientSerializer
 from recipe.serializers import RecipeDetailSerializer
+from recipe.serializers import RecipeImageSerializer
 from recipe.serializers import RecipeSerializer
 from recipe.serializers import TagSerializer
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import Serializer
 from rest_framework.viewsets import GenericViewSet
@@ -60,9 +65,26 @@ class RecipeViewSet(ModelViewSet):
     def get_serializer_class(self) -> type[Serializer]:
         if self.action == "retrieve":
             return RecipeDetailSerializer
+        elif self.action == "upload_image":
+            return RecipeImageSerializer
         else:
             return cast(type[Serializer], self.serializer_class)
 
     @beartype
     def perform_create(self, serializer: RecipeSerializer) -> None:  # type: ignore
         _ = serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    @beartype
+    def upload_image(
+        self, request: Request, _pk: str | None = None
+    ) -> Response:
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
