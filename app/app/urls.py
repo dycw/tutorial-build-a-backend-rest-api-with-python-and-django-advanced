37a67b380d17a561d04a8881718c69198c50ed2c
Derek Wan <d.wan@icloud.com>
2022-04-29 23:51:14 +0800

    update dockerfile

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,10 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.admin import site
+from django.urls import URLPattern
+from django.urls import URLResolver
 from django.urls import include
 from django.urls import path
 
 
-urlpatterns = [
+urlpatterns: list[URLResolver | URLPattern] = [
     path("admin/", site.urls),
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
 ]
+urlpatterns.extend(
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
